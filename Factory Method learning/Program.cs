90d8d5f6a7e5824371a8431a03c0f8979befb86e
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-08-03 22:59:52 +0300

    Добавьте файлы проекта.

---
+++ Factory Method learning/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_Method_learning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Driver driver = new EarthTransportDriver("Вадим", "2В");
+            Transport transport1 = driver.Create();
+
+            driver = new WaterTransportDriver("Максим", "Капитан");
+            Transport transport = driver.Create();
+
+            Console.ReadLine();
+        }
+    }
+    // abstract class for a driver
+    abstract class Driver
+    {
+        public string Name { get; set; }
+        public string Specification { get; set; }
+
+        public Driver(string name, string specification)
+        {
+            Name = name;
+            Specification = specification;
+        }
+        // fabric method
+        abstract public Transport Create();
+    }
+    // drive earth transport
+    class EarthTransportDriver : Driver
+    {
+        public EarthTransportDriver(string name, string specification) : base(name,specification)
+        { }
+
+        public override Transport Create()
+        {
+            return new EarthTransport();
+        }
+    }
+    // drive water transport
+    class WaterTransportDriver : Driver
+    {
+        public WaterTransportDriver(string name, string specification) : base(name, specification)
+        { }
+
+        public override Transport Create()
+        {
+            return new WaterTransport();
+        }
+    }
+
+    abstract class Transport
+    {
+
+    }
+
+    class EarthTransport : Transport
+    {
+        public EarthTransport()
+        {
+            Console.WriteLine("Rides earth transport");
+        }
+    }
+    class WaterTransport : Transport
+    {
+        public WaterTransport()
+        {
+            Console.WriteLine("Rides water transport");
+        }
+    }
+
+}
